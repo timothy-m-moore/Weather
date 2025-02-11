618acbb04c5f0a2d80ef4fa436a727e9578ea7ca
Tim Moore <timothy.m.moore@icloud.com>
2025-02-10 23:10:09 -0500

    Implemented Serilog logging

--- WeatherOpenTel/Program.cs
+++ WeatherOpenTel/Program.cs
@@ -1,4 +1,22 @@
+using System.Diagnostics.Eventing.Reader;
+using Serilog;
+using Serilog.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Serilog from appsettings.json
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .Enrich.WithThreadId()
+    .WriteTo.File(
+        path: "logs/log-.txt",
+        rollingInterval: RollingInterval.Day,
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3} {ThreadId}] {Properties:j}{Message:lj}{NewLine}{Exception}")
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Properties:j}{Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -19,8 +37,11 @@ var summaries = new[]
     "Testing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", (ILogger<WeatherForecast> logger) =>
     {
+        using(LogContext.PushProperty("JobRequestId: {JobRequestId}", Guid.NewGuid()));
+        logger.LogInformation("New request for weather forecast");
+        
         var forecast = Enumerable.Range(1, 5).Select(index =>
                 new WeatherForecast
                 (
@@ -29,6 +50,7 @@ app.MapGet("/weatherforecast", () =>
                     summaries[Random.Shared.Next(summaries.Length)]
                 ))
             .ToArray();
+        logger.LogInformation("Returning weather forecast: {Forecast}", forecast);
         return forecast;
     })
     .WithName("GetWeatherForecast");

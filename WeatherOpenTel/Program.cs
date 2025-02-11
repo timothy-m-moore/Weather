b6a509e3ed7bd2bf8697deffbd4df976ae2f0bc6
Tim Moore <timothy.m.moore@icloud.com>
2025-02-10 21:19:40 -0500

    Test commit

--- WeatherOpenTel/Program.cs
+++ WeatherOpenTel/Program.cs
@@ -16,7 +16,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Testing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
